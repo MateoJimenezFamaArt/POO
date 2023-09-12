1052e27a2b437a02880b559ccb549cf288fd4501
2CB <mateojimenezfamaart@gmail.com>
2023-09-11 19:30:24 -0500

    Primeros Arreglos de Clases

--- ProjectoFinalPoo/ProjectoFinalPoo/Granjero.cs
+++ ProjectoFinalPoo/ProjectoFinalPoo/Granjero.cs
@@ -8,5 +8,16 @@ namespace ProjectoFinalPoo
 {
     internal class Granjero
     {
+        private void TrabajarEnElCampo()
+        {
+            Console.WriteLine("El sol ya ha salido, te das una buena estirada y te preparas para salir al campo");
+            Console.WriteLine("Con un fuerte suspiro te levantas y vislumbras el campo, ha llegado el momento.");
+            Console.WriteLine("Es hora de trabajar...");
+            Console.WriteLine("Vas a preparar el campo para trabjar en Trigo o en Papas?");
+            Console.WriteLine("1 = SI /// 2 = NO");
+
+
+        }
+
     }
 }
