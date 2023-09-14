f877286d3aff5624191727e783ebaadf37661585
2CB <mateojimenezfamaart@gmail.com>
2023-09-13 19:19:21 -0500

    Trabajar en el campo metodo

--- ProjectoFinalPoo/ProjectoFinalPoo/Granjero.cs
+++ ProjectoFinalPoo/ProjectoFinalPoo/Granjero.cs
@@ -8,14 +8,19 @@ namespace ProjectoFinalPoo
 {
     internal class Granjero
     {
-        private void TrabajarEnElCampo()
+        public void TrabajarEnElCampo()
         {
             Console.WriteLine("El sol ya ha salido, te das una buena estirada y te preparas para salir al campo");
             Console.WriteLine("Con un fuerte suspiro te levantas y vislumbras el campo, ha llegado el momento.");
             Console.WriteLine("Es hora de trabajar...");
             Console.WriteLine("Vas a preparar el campo para trabjar en Trigo o en Papas?");
             Console.WriteLine("1 = SI /// 2 = NO");
+            int InstantResponse = 0;
+            InstantResponse = int.Parse(Console.ReadLine());
 
+            if(InstantResponse != 1 || InstantResponse != 2) { Console.WriteLine("Porfavor haz un input congruente"); InstantResponse = int.Parse(Console.ReadLine());}
+            if (InstantResponse == 1) { Console.WriteLine("Preparandote para salir al campo tomas la hoz para trabajar en trigos lo jornada sera larga pero valdra la pena."); }
+            if (InstantResponse == 2) { Console.WriteLine("Preparandote para salir al campo tomas los guantes y la azada para salir a trabajar en las papas, la jornada sera larga pero valdra la pena"); }
 
         }
 
--- ProjectoFinalPoo/ProjectoFinalPoo/Program.cs
+++ ProjectoFinalPoo/ProjectoFinalPoo/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Vamos a realizar la creacion de clases");
+
+            Granjero g1 = new Granjero();
+            g1.TrabajarEnElCampo();
         }
     }
 }
