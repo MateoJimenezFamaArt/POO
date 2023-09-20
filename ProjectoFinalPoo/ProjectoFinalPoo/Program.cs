24b439871dda4798f99b6a0c3e542699e595a9e9
2CB <mateojimenezfamaart@gmail.com>
2023-09-19 19:35:09 -0500

    Avances pequenos

--- ProjectoFinalPoo/ProjectoFinalPoo/Program.cs
+++ ProjectoFinalPoo/ProjectoFinalPoo/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ProjectoFinalPoo
+﻿using System.Numerics;
+
+namespace ProjectoFinalPoo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Vamos a realizar la creacion de clases");
+            Console.WriteLine("Hola, bienvenido a farm simulator Console Edition TM.");
+            Console.WriteLine("Porfavor dime cuantos dias deseas jugar?");
+            Console.WriteLine("(Se recomienda jugar unos 10 dias para experimentar el juego por completo)");
+            int Daystoplay = 0;
+            Daystoplay = int.Parse (Console.ReadLine());
+
+
 
             Granjero g1 = new Granjero();
             g1.TrabajarEnElCampo();
